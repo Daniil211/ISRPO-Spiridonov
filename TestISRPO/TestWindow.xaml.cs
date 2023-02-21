124b91d56f95cc7205a6ff98731fe09d3aab9cd5
Daniil211 <id20214@gmail.com>
2023-02-21 18:05:59 +0300

    Last commit

--- TestISRPO/TestWindow.xaml.cs
+++ TestISRPO/TestWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace TestISRPO
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
